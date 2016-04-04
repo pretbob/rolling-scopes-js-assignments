2da8881bff8efb50d2bee9c5f096584406f54d7e
Vasiliy Yakubuk <profmed.economist@gmail.com>
2016-04-04 21:18:10 +0300

    05 regexp

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -54,7 +54,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-   return /pit|[s].+/;
+   return /pit|^[sr].+/;
 }
 
 
@@ -73,7 +73,7 @@ function getRegexForPitSpot() {
  * @return {RegExp}
  */
 function getRegexForIPv4() {
-   throw new Error('Not implemented');
+  return /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
 }
 
 
@@ -92,7 +92,7 @@ function getRegexForIPv4() {
  * @return {RegExp}
  */
 function getRegexForSSN() {
-   throw new Error('Not implemented');
+   return /(?!000)\d{3}-(?!00)\d{2}-(?!0000)\d{4}/;
 }
 
 
@@ -117,7 +117,7 @@ function getRegexForSSN() {
  *   'Pa55'.match(validator) => false
  */
 function getPasswordValidator(minLength) {
-   throw new Error('Not implemented');
+    return new RegExp("^((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[^\\W_]{"+minLength+",})");
 }
 
 
